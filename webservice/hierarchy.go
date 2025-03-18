b4e20e536d399d329aaf9fb50e82d30362dade30
Vladyslav Platonov <vladyslav.platonov@gmail.com>
2025-03-18 19:05:24 +0100

    feat: hierarchy webservice

---
+++ webservice/hierarchy.go
@@ -0,0 +1,31 @@
+package webservice
+
+import (
+	"context"
+)
+
+const pathHierarchy = "/hierarchyJSON"
+
+type HierarchyRequest struct {
+	// ID the geonameId for the hierarchy
+	ID uint64 `url:"geonameId"`
+}
+
+// Hierarchy returns a list of GeoName records, ordered by hierarchy level.
+// The top hierarchy (continent) is the first element in the list.
+func (c *Client) Hierarchy(ctx context.Context, req HierarchyRequest) ([]GeoName, error) {
+	var res struct {
+		Items []GeoName `json:"geonames"`
+	}
+
+	if err := c.apiRequest(
+		ctx,
+		pathHierarchy,
+		req,
+		&res,
+	); err != nil {
+		return nil, err
+	}
+
+	return res.Items, nil
+}
